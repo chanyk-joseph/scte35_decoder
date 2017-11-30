3727dc30a0a9b7169c8032f56dcdb81f63d767c9
Joseph Chan <chanyk.joseph@gmail.com>
2017-11-30 14:50:49 +0800

    update

--- common/parser.go
+++ common/parser.go
@@ -11,14 +11,14 @@ type SCTE35 struct {
 	TableID                uint8  `json:"table_id"`
 	SectionSyntaxIndicator bool   `json:"section_syntax_indicator"`
 	PrivateIndicator       bool   `json:"private_indicator"`
-	SectionLength          uint16 `json:"section_length"` //12 bits
+	SectionLength          uint16 `json:"section_length"` // 12 bits
 	ProtocolVersion        uint8  `json:"protocol_version"`
 	EncryptedPacket        bool   `json:"encrypted_packet"`
-	EncryptionAlgorithm    byte   `json:"encryption_algorithm"` //6 bits
-	PTSAdjustment          uint64 `json:"pts_adjustment"`       //33 bits
+	EncryptionAlgorithm    byte   `json:"encryption_algorithm"` // 6 bits
+	PTSAdjustment          uint64 `json:"pts_adjustment"`       // 33 bits
 	CWIndex                uint8  `json:"cw_index"`
-	Tier                   uint16 `json:"tier"`                  //12 bits
-	SpliceCommandLength    uint16 `json:"splice_command_length"` //12 bits
+	Tier                   uint16 `json:"tier"`                  // 12 bits
+	SpliceCommandLength    uint16 `json:"splice_command_length"` // 12 bits
 	SpliceCommandType      byte   `json:"splice_command_type"`
 
 	DescriptorLoopLength uint16             `json:"descriptor_loop_length"`
